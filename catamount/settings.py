703eb4bcded57c441339feded4331a19f5ec051f
Mario Bailey <romario.bailey@icloud.com>
2020-10-01 13:09:18 -0400

    added django heroku

--- catamount/settings.py
+++ catamount/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-
+import django_heroku
 from pathlib import Path
 
 
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$_^@0z@2xte8b&-@^+15fo=4^f!63dlo4t%v@h5_m^!(ektged'
-
+# SECRET_KEY = '$_^@0z@2xte8b&-@^+15fo=4^f!63dlo4t%v@h5_m^!(ektged'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -148,3 +148,5 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
 	messages.ERROR: 'danger'
 }
+
+django_heroku.settings(locals())
